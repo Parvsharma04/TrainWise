6be0676f7e771aa4ccbd1ef77c2fdca6b3079d18
Avi Rana <itzavi1718@gmail.com>
2025-02-21 09:48:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -79,7 +79,7 @@ io.on("connection", (socket) => {
 });
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 server.listen(port, () => {
     console.log("Listening on " + port);
 });
