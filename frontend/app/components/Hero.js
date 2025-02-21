9b2eb313cc8ee1eae1ddd8a825cde6f770356f25
Avi Rana <itzavi1718@gmail.com>
2025-02-21 10:45:16 +0530

    redone

--- frontend/app/components/Hero.js
+++ frontend/app/components/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, setHasClicked] = useState(false);
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [loadedVideos, setLoadedVideos] = useState(0);
 
   const totalVideos = 4;
@@ -85,16 +85,6 @@ const Hero = () => {
 
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      {loading && (
-        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
-          <div className="three-body">
-            <div className="three-body__dot"></div>
-            <div className="three-body__dot"></div>
-            <div className="three-body__dot"></div>
-          </div>
-        </div>
-      )}
 
       <div
         id="video-frame"
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,19 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Features from "./components/Features";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
-    <>
-    <NavBar />
-    <Hero />
-    </>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Contact />
+      {/*
+      <Footer /> */}
+    </main>
   )
 }
