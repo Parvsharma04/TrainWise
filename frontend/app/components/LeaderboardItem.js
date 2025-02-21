d55929a15215d585441e09e96aeb6f6bc66bc286
Avi Rana <itzavi1718@gmail.com>
2025-02-21 11:56:22 +0530

    Update LeaderboardItem.js

--- frontend/app/components/LeaderboardItem.js
+++ frontend/app/components/LeaderboardItem.js
@@ -2,7 +2,7 @@ export default function LeaderboardItem({rank, name, score}) {
     return (
         <li className="flex justify-between mb-2">
                 <div>
-                <span className="inline-flex items-center justify-center w-8 h-8 text-sm font-semibold rounded-full">
+                <span className="inline-flex items-center justify-center text-sm font-semibold">
                       {rank}
                 </span>
                 <span className="ml-3">{name}</span>
