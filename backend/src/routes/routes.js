27c638363f4d6d0f2f33809d85f772434a08cad7
Arshdeep-13 <arshdeeprooprai@gmail.com>
2025-02-21 10:55:09 +0530

    router folder init

---
+++ backend/src/routes/routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("server is running and up");
+});
+router.get("/health", (res, res) => {
+  res.send("server is healthy");
+});
+router.post("/signup", (req, res) => {});
+router.post("/login", (req, res) => {});
+
+module.exports = router;
